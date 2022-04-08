961ee505655e1547c6f97b2de6159377dc1c7166
tamikaramkissoon <tamika.ramkissoon@my.uwi.edu>
2022-04-08 03:13:23 +0000

    Built models

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,1 +1,3 @@
 from .user import *
+from .myGame import *
+from .community import *
---
+++ App/models/community.py
@@ -0,0 +1,20 @@
+from App.database import db
+
+class Community(db.Model):
+    communityId = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    gameId =  db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    
+
+    def __init__(self, gameId):
+        self.gameId = gameId
+
+    def toDict(self):
+        return{
+            'id': self.communityId,
+            'name': self.name,
+            'gameId': self.gameId
+           
+        }
+
+    
