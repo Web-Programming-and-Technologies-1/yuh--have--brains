9f12a58c2fbe7d45236ca7e6cb6f697baabe6429
tamikaramkissoon <tamika.ramkissoon@my.uwi.edu>
2022-04-12 20:32:06 +0000

    update_models

--- App/models/community.py
+++ App/models/community.py
@@ -1,20 +1,20 @@
-from App.database import db
+# from App.database import db
 
-class Community(db.Model):
-    communityId = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False, unique=True)
-    gameId =  db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+# class Community(db.Model):
+#     communityId = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(120), nullable=False, unique=True)
+#     gameId =  db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
     
 
-    def __init__(self, gameId):
-        self.gameId = gameId
+#     def __init__(self, gameId):
+#         self.gameId = gameId
 
-    def toDict(self):
-        return{
-            'id': self.communityId,
-            'name': self.name,
-            'gameId': self.gameId
+#     def toDict(self):
+#         return{
+#             'id': self.communityId,
+#             'name': self.name,
+#             'gameId': self.gameId
            
-        }
+#         }
 
     
--- App/models/myGame.py
+++ App/models/myGame.py
@@ -1,20 +1,20 @@
-from App.database import db
+# from App.database import db
 
-class MyGame(db.Model):
-    gameId = db.Column(db.Integer, primary_key=True)
-    userId =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    score = db.Column(db.Interger, nullable=False)
-    community = db.relationship('Community', backref='Mygame', lazy=True, cascade="all, delete-orphan")
+# class MyGame(db.Model):
+#     gameId = db.Column(db.Integer, primary_key=True)
+#     userId =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     score = db.Column(db.Interger, nullable=False)
+#     community = db.relationship('Community', backref='Mygame', lazy=True, cascade="all, delete-orphan")
     
-    def __init__(self, userId, score = 0):
-        self.userId = userId
-        self.score = score
+#     def __init__(self, userId, score = 0):
+#         self.userId = userId
+#         self.score = score
 
-    def toDict(self):
-        return{
-            'gameId': self.gameId,
-            'userId': self.userId,
-            'score': self.score
-        }
+#     def toDict(self):
+#         return{
+#             'gameId': self.gameId,
+#             'userId': self.userId,
+#             'score': self.score
+#         }
 
     
