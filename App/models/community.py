42f387733739704db41ce2e67324cf61aa586281
tamikaramkissoon <tamika.ramkissoon@my.uwi.edu>
2022-04-17 23:41:47 +0000

    updated_models

--- App/models/community.py
+++ App/models/community.py
@@ -1,21 +1,21 @@
-# from App.database import db
+from App.database import db
 
-# class Community(db.Model):
-#     communityId = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(120), nullable=False, unique=True)
-#     # gameId =  db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
-#     Users = db.relationship('User', backref=db.backref('community', lazy='joined'))
+class Community(db.Model):
+    communityId = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    # gameId =  db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    Users = db.relationship('User', backref=db.backref('community', lazy='joined'))
     
 
-#     def __init__(self, gameId):
-#         self.gameId = gameId
+    def __init__(self, gameId):
+        self.gameId = gameId
 
-#     def toDict(self):
-#         return{
-#             'id': self.communityId,
-#             'name': self.name,
-#             'gameId': self.gameId,
-#            'users': [user.toDict() for user in self.Users]
-#         }
+    def toDict(self):
+        return{
+            'id': self.communityId,
+            'name': self.name,
+            'gameId': self.gameId,
+           'users': [user.toDict() for user in self.Users]
+        }
 
     
--- App/models/myGame.py
+++ App/models/myGame.py
@@ -1,20 +1,20 @@
-# from App.database import db
+from App.database import db
 
-# class MyGame(db.Model):
-#     gameId = db.Column(db.Integer, primary_key=True)
-#     userId =  db.Column(db.Integer, db.ForeignKey('user.userId'), nullable=False)
-#     score = db.Column(db.Integer, nullable=False)
+class MyGame(db.Model):
+    gameId = db.Column(db.Integer, primary_key=True)
+    userId =  db.Column(db.Integer, db.ForeignKey('user.userId'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    leaderBoard = db.relationship('Leaderboard', backref=db.backref('leaderboard', lazy='joined'))
     
-    
-#     def __init__(self, userId, score = 0):
-#         self.userId = userId
-#         self.score = score
+    def __init__(self, userId, score = 0):
+        self.userId = userId
+        self.score = score
 
-#     def toDict(self):
-#         return{
-#             'gameId': self.gameId,
-#             'userId': self.userId,
-#             'score': self.score
-#         }
+    def toDict(self):
+        return{
+            'gameId': self.gameId,
+            'userId': self.userId,
+            'score': self.score
+        }
 
     
