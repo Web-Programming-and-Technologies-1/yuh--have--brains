9f395bb8f83454ed7fb93bd67c8741fc7fe0dc4e
Tamika Ramkissoon <77646306+tamikaramkissoon@users.noreply.github.com>
2022-04-20 20:37:42 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://pogsykejqtotng:40b33b7a6eeb4c0d309c1377058603453dcc177569d00daff8f32a24ae1cf862@ec2-34-192-210-139.compute-1.amazonaws.com:5432/decki791sakjcd"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
