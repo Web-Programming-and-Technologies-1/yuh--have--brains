3440a293e14d4bb7876bbe31565347f2fe5c2fce
tamikaramkissoon <tamika.ramkissoon@my.uwi.edu>
2022-04-21 18:55:32 +0000

    heroku_update

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://ibpxurhincftux:1ad22731aef527b330389c5c2ce01a3804349ba1f10d890f2533ba2e94ab83ed@ec2-3-218-171-44.compute-1.amazonaws.com:5432/d6fdtnpih1gdp1"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
